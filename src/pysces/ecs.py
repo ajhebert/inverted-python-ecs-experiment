6a7d7bda113435c9e6961ebb05820c9e5f87beca
Andrew Hebert Jr <gingergiant.me@gmail.com>
2022-03-22 13:20:06 -0500

    Fleshed out base ECS, added docstrings, test

---
+++ src/pysces/ecs.py
@@ -0,0 +1,133 @@
+from abc import ABC, abstractmethod
+import secrets
+from pprint import pprint
+from collections import defaultdict
+from dataclasses import dataclass, field, fields
+from typing import Optional
+
+
+@dataclass
+class Component:
+    """
+    Components are the building blocks of compositional objects,
+    a.k.a., Entities. Components are comprised of data fields,
+    and lack methods of any kind except, perhaps, factories.
+    
+    All Components (and subclasses) _must_ be dataclasses.
+    """
+    _registry = defaultdict(
+        list
+    )  # Only meant to be accessed outside of class by Systems
+    _entity: Optional[str] = field(
+        default=None, init=False
+    )  # id of instantiating entity
+
+    @classmethod
+    @property
+    def all(cls) -> dict:
+        return Component._registry
+
+    @classmethod
+    @property
+    def registry(cls) -> any:
+        return Component._registry[cls.__name__]
+
+    @property
+    def entity(self):
+        """
+        The entity which is composed with this component.
+        
+        a.k.a., the 'parent' entity
+        """
+        return self._entity
+
+    @entity.setter
+    def entity(self, value):
+        assert self._entity == None, f"Changing parent entity not allowed"
+        self._entity = value
+        if value not in self.registry:
+            self.registry.append(value)
+
+
+@dataclass
+class Entity:
+    """
+    Entities bind Components together into cohesive ideas relevant
+    to Systems. 
+    
+    All Entities (and subclasses) _must_ be dataclasses.
+    """
+    def new_token():
+        token = secrets.token_hex(16)
+        while token in Entity._registry:
+            token = secrets.token_hex(16)
+        return token
+
+    _registry = defaultdict(None)
+    id: str = field(default_factory=new_token)
+
+    @classmethod
+    @property
+    def registry(cls) -> any:
+        """
+        Entities are registered by their unique token
+        so Systems can quickly look up an Entity from
+        a Component's entity (:str).
+
+        Returns:
+            dict of id:Entity (key:value) pairs
+        """
+        return Entity._registry
+
+    def __post_init__(self, *args, **kwargs):
+        for field_ in fields(self):
+            if Component in field_.type.__mro__:
+                component = getattr(self, field_.name)
+                component.entity = self.id
+        self.registry[self.id] = self
+        self.__post_reg__(*args, **kwargs)
+        
+    def __post_reg__(self):
+        """
+        Post-registration, any class which inherits from Entity
+        and needs to 'initialize' its attributes should write 
+        this function.
+        
+        Allegory:
+        __init__        => class
+        __post_init__   => dataclass
+        __post_reg__    => Entity
+        
+        """
+        ...
+
+
+class System(ABC):
+    @property
+    @abstractmethod
+    def components() -> list[Component]:
+        """
+        The 'list' of components, a System may operate on only
+        those entities which possess all listed Components.
+        """
+        ...
+
+    @classmethod
+    @property
+    def entities(cls) -> list[str]:
+        """All Entities composed with all components attributed to this System.
+
+        Returns:
+            list[str]: list of Entities possessing all components required
+                        by this System
+        """
+        assert len(cls.components) > 0
+        index = cls.components[0].__name__
+        if len(cls.components) == 1:
+            return Component.all[index]
+        else:
+            result = Component.all[index]
+            for component in cls.components:
+                index = component.__name__
+                result.append([x for x in result if x in Component.all[index]])
+            return result
---
+++ src/pysces/test_system.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from pysces.ecs import Entity, Component, System
+
+
+class ComponentSys(System):
+    """
+    This System tracks all entities which are composed
+    with the base Component class.
+    """
+
+    components = [Component]
+
+
+@dataclass
+class MyEntity(Entity):
+    one: Component = Component()
+    two: Component = Component()
+    three: Component = Component()
+
+
+def test_component_system():
+    myEntity = MyEntity(id="deadbeef")
+    print(myEntity)
+    assert len(ComponentSys.entities) == 1
+    assert ComponentSys.entities[0] == "deadbeef"
